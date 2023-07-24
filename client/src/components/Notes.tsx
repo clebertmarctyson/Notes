b70d5bd130c1c4bf968bd3e3a5a928589328eaa5
clebertmarctyson <marctysonclebert9@gmail.com>
2023-07-23 22:53:14 -0400

    Fixing adding dist folder

--- client/src/components/Notes.tsx
+++ client/src/components/Notes.tsx
@@ -18,7 +18,7 @@ const Notes = () => {
     <div className="flex-1 md:flex-none md:border-r-2 p-4 pb-8 w-full h-auto md:h-screen md:w-[20rem] overflow-hidden">
       <h1 className="font-bold text-3xl my-4">Notes</h1>
       {isLoading ? (
-        <Loading size={"loading-lg"} />
+        <Loading classes={"loading-lg"} />
       ) : state.notes.length === 0 ? (
         <p className="font-bold text-3xl text-gray-400 text-center mt-16">
           No Note Found
